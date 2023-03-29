6ab6900cb911959d5450c1c70a1a0ff365a5df69
G Preetham (Nokia) <g.preetham@nokia.com>
2023-03-29 23:08:46 +0530

    APIs added for DELETE todos and single todo based on Id

--- Project-002-todo-app-with-rest-api/main.go
+++ Project-002-todo-app-with-rest-api/main.go
@@ -18,16 +18,60 @@ Goal is to have the following functionality along with rest api support
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
+type todo struct {
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+	Done bool   `json:"done"`
+}
+
+var completeTodo = []todo{
+	{1, "a1", false},
+	{2, "a2", false},
+}
+
 func getTodoAll(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "Here goes the list of all todos")
+	c.IndentedJSON(http.StatusOK, completeTodo)
 }
 
 func getTodo(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "Here goes the list of single todo"+string(c.Param("id")))
+	todoId, err := strconv.ParseInt(c.Param("id"), 10, 64)
+
+	if err == nil {
+		for _, td := range completeTodo {
+			if td.Id == todoId {
+				c.IndentedJSON(http.StatusOK, td)
+				return
+			}
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, "Id not Found 404")
+}
+
+func delteTodoAll(c *gin.Context) {
+	completeTodo = []todo{}
+	c.IndentedJSON(http.StatusOK, completeTodo)
+}
+
+func delteTodo(c *gin.Context) {
+	todoId, err := strconv.ParseInt(c.Param("id"), 10, 64)
+
+	if err == nil {
+		for i, td := range completeTodo {
+			if td.Id == todoId {
+				completeTodo[i] = completeTodo[0]
+				completeTodo = completeTodo[1:]
+
+				c.IndentedJSON(http.StatusOK, "Deleted todo "+td.Name)
+				return
+			}
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, "Id not Found to delete 404")
 }
 
 func main() {
@@ -35,6 +79,10 @@ func main() {
 	r := gin.New()
 	r.GET("/todo", getTodoAll)
 	r.GET("/todo/:id", getTodo)
+
+	r.DELETE("/todo", delteTodoAll)
+	r.DELETE("/todo/:id", delteTodo)
+
 	r.Run(":8086")
 
 }
